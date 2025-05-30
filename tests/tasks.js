296ab4dcd1200e603da8c7df888af620a40a6242
Pyslar Maksym <aronthomasmiller@gmail.com>
2025-05-30 14:12:57 +0300

    if else tasks

---
+++ tests/tasks.js
@@ -0,0 +1,101 @@
+//---------------------
+//task 1
+//---------------------
+// function isEven(number) {
+//   if (number % 2 === 0) {
+//     console.log("number is even");
+//   } else {
+//     console.log("number is odd");
+//   }
+// }
+
+// isEven(2);
+
+//---------------------
+//task2
+//---------------------
+// function hello(time) {
+//   if (time < 12) {
+//     console.log("good morning");
+//   } else if (time >= 12 && time <= 18) {
+//     console.log("good afternoon");
+//   } else {
+//     console.log("good evening");
+//   }
+// }
+
+// hello(18)
+
+//---------------------
+//task3
+//---------------------
+// function verifyScore(score) {
+//   if (score >= 50) {
+//     console.log("test passed");
+//   } else {
+//     console.log("test failed");
+
+//   }
+// }
+
+// verifyScore(42)
+
+//---------------------
+//task4
+//---------------------
+// function canVote(age) {
+//   if (age >= 18) {
+//     console.log("you can vote");
+//   } else {
+//     console.log("you can't vote");
+//   }
+// }
+
+// canVote(11)
+
+// verifyScore(42)
+
+//---------------------
+// task5
+//---------------------
+// function compareNumbers(number1, number2) {
+//   if (number1 > number2) {
+//     console.log("first number is bigger");
+//   } else if (number1 < number2) {
+//     console.log("second number is bigger");
+//   } else {
+//     console.log("the numbers are the same");
+//   }
+// }
+
+// compareNumbers(1, 3);
+
+//---------------------
+// task6
+//---------------------
+// function trafficLight(color) {
+//   if (color === "green") {
+//     console.log("go");
+//   } else if (color === "yellow") {
+//     console.log("prepare");
+//   } else {
+//     console.log("stop");
+//   }
+// }
+
+// trafficLight("green");
+
+//---------------------
+// task7
+//---------------------
+
+// function getNumberType(num) {
+//   if (num > 0) {
+//     console.log("number is positive");
+//   } else if (num < 0) {
+//     console.log("number is negative");
+//   } else {
+//     console.log("number is 0");
+//   }
+// }
+// getNumberType(0);
